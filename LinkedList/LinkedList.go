4ae0584224f5de0516107ee06a0bd3efedfb0f62
Your Name <55reshetnikov@mail.ru>
2024-12-28 13:30:10 +0500

    Скачали библиотеку и подготовили тесты

--- LinkedList/LinkedList.go
+++ LinkedList/LinkedList.go
@@ -1,9 +1,14 @@
 package linkedlist
 
 type LinkedList[T any] struct {
+	Node *Node[T]
 }
 
 type Node[T any] struct {
 	data T
 	next *Node[T]
+}
+
+func NewLinkedList[T any]() *LinkedList[T] {
+	return &LinkedList[T]{}
 }
