7b8a761a6ce3e23a7fb085e2a2583077ef271446
Your Name <55reshetnikov@mail.ru>
2025-01-11 18:14:18 +0500

    1

---
+++ LinkedList/DoublyLinkedList.go
@@ -0,0 +1,95 @@
+package linkedlist
+
+type Node1[T any] struct {
+	data T
+	prev *Node1[T]
+	next *Node1[T]
+}
+
+type DoublyLinkedList[T any] struct {
+	Count int
+	tail  *Node1[T]
+	head  *Node1[T]
+}
+
+func NewLinkedList1[T any]() *DoublyLinkedList[T] {
+	return &DoublyLinkedList[T]{}
+}
+
+// Метод добавление в начало.
+func (doublyLinkedList *DoublyLinkedList[T]) AddingBeginning(data T) {
+	newNode := &Node1[T]{
+		data: data,
+	}
+	if doublyLinkedList.head == nil {
+		doublyLinkedList.head = newNode
+		doublyLinkedList.tail = newNode
+	} else {
+		newNode.next = doublyLinkedList.head
+		doublyLinkedList.head.prev = newNode
+		doublyLinkedList.head = newNode
+	}
+	doublyLinkedList.Count++
+}
+
+// Метод добавление в конец.
+func (doublyLinkedList *DoublyLinkedList[T]) AddingAtEnd(data T) {
+	newNode := &Node1[T]{
+		data: data,
+	}
+	if doublyLinkedList.head == nil {
+		doublyLinkedList.head = newNode
+		doublyLinkedList.tail = newNode
+	} else {
+		one := doublyLinkedList.head
+		for one.next != nil {
+			one = one.next
+		}
+		newNode.prev = one
+		one.next = newNode
+		doublyLinkedList.tail = newNode
+	}
+	doublyLinkedList.Count++
+}
+
+// Метод удаления из начало.
+func (doublyLinkedList *DoublyLinkedList[T]) RemovalBeginning() {
+	if doublyLinkedList.head == nil {
+		return
+	}
+	if doublyLinkedList.head.next == nil {
+		doublyLinkedList.head = nil
+		doublyLinkedList.tail = nil
+	} else {
+		doublyLinkedList.head = doublyLinkedList.head.next
+		doublyLinkedList.head.prev = nil
+	}
+	doublyLinkedList.Count--
+}
+
+// Метод удаления из конца.
+func (doublyLinkedList *DoublyLinkedList[T]) RemovalEnd() {
+	if doublyLinkedList.tail == nil {
+		return
+	}
+	if doublyLinkedList.tail.next == nil {
+		doublyLinkedList.head = nil
+		doublyLinkedList.tail = nil
+	} else {
+		doublyLinkedList.tail = doublyLinkedList.tail.next
+		doublyLinkedList.tail.next = nil
+	}
+	doublyLinkedList.Count--
+}
+
+/* Метод узла по значению.
+func (doublyLinkedList *DoublyLinkedList[T]) Search(data T) *Node1[T] {
+	number := doublyLinkedList.head
+	for number != nil {
+		if {
+			return number
+		}
+		number = number.next // Исправлено
+	}
+	return nil
+}*/
--- LinkedList_test.go
+++ LinkedList_test.go
@@ -5,7 +5,7 @@ import (
 
 	//github.com/rESh211/Data-struct.git"
 
-	linkedlist "github.com/rESh211/Data-struct.git/LinkedList"
+	linkedlist "github.com/rESh211/Data-struct/LinkedList"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,5 +31,13 @@ func TestGet(t *testing.T) {
 		list.Push(251)
 		ferst := list.Get(0)
 		assert.Equal(251, *ferst, "проверка первого элемента")
+		list.Push(52)
+		second := list.Get(1)
+		assert.Equal(52, *second, "проверка второго элемента")
+		list.Push(487)
+		third := list.Get(2)
+		assert.Equal(487, *third, "проверка третьего элемента")
+		second = list.Get(1)
+		assert.Equal(52, *second, "повторная проверка второго элемента")
 	})
 }
